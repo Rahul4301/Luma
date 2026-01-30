6c0ff5b9e3b407157ab74fe6f003aa78a85cc05e
Rahul Suthar <Suthar.rahul042@gmail.com>
2026-01-29 23:34:50 -0800

    Initial Commit

---
+++ Luma/LumaApp.swift
@@ -0,0 +1,17 @@
+//
+//  LumaApp.swift
+//  Luma
+//
+//  Created by Rahul Suthar on 1/29/26.
+//
+
+import SwiftUI
+
+@main
+struct LumaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
